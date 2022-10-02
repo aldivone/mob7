8afed50cfa9c1b822b4936a0f25a39f21f3f9028
aldivone <aldivone@gmail.com>
2022-10-02 10:03:15 -0300

    feat: adicionado os sequences das entidades geolocalização e ponto de interesse

--- src/main/java/br/com/localiza/mob7/geolocalizacao/Geolocalizacao.java
+++ src/main/java/br/com/localiza/mob7/geolocalizacao/Geolocalizacao.java
@@ -3,7 +3,10 @@ package br.com.localiza.mob7.geolocalizacao;
 import java.util.Date;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
 
 import lombok.AllArgsConstructor;
@@ -20,6 +23,8 @@ import lombok.NoArgsConstructor;
 public class Geolocalizacao {
 
 	@Id
+	@SequenceGenerator(name = "SEQ_GEOLOCALIZACAO", sequenceName = "SEQ_GEOLOCALIZACAO", allocationSize = 1)
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "SEQ_GEOLOCALIZACAO")
 	private Long id;
 
 	private String placa;
--- src/main/java/br/com/localiza/mob7/pontodeinteresse/PontoDeInteresse.java
+++ src/main/java/br/com/localiza/mob7/pontodeinteresse/PontoDeInteresse.java
@@ -1,7 +1,10 @@
 package br.com.localiza.mob7.pontodeinteresse;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
 
 import lombok.AllArgsConstructor;
@@ -18,6 +21,8 @@ import lombok.NoArgsConstructor;
 public class PontoDeInteresse {
 
 	@Id
+	@SequenceGenerator(name = "SEQ_PONTO_INTERESSE", sequenceName = "SEQ_PONTO_INTERESSE", allocationSize = 1)
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "SEQ_PONTO_INTERESSE")
 	private Long id;
 
 	private String nome;
